beb68fd94b666c14c82dcd61b61d7acc3fc38459
Thomas Teflon Ted Davis <github@rudiment.net>
2009-05-06 20:28:01 -0400

    mapped out the UI design and created some empty controllers

---
+++ Classes/SettingsViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.h
+//  JustTweet Lite
+//
+//  Created by Ted on 5/6/09.
+//  Copyright 2009 Anachromystic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingsViewController : UIViewController {
+
+}
+
+@end
---
+++ Classes/TweetViewController.h
@@ -0,0 +1,16 @@
+//
+//  TweetViewController.h
+//  JustTweet Lite
+//
+//  Created by Ted on 5/6/09.
+//  Copyright 2009 Anachromystic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TweetViewController : UIViewController {
+
+}
+
+@end
